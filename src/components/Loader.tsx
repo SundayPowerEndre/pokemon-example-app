08fa26a315013705e8b8d9a58c45aa2ccf65c63a
SundayPowerEndre <sundaypeace@duck.com>
2023-12-28 21:30:36 +0100

    Adds new loader

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,47 +3,17 @@ import { Dialog } from "./ui/dialog";
 
 const MainLoader = ({ className }: { className?: string }) => {
   return (
-    <div className={cn("flex h-screen items-center justify-center", className)}>
-      <div className="animate-spin-slow">
-        <svg
-          className=" h-96 w-96"
-          viewBox="0 0 100 100"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <defs>
-            <clipPath id="half">
-              <rect x="0" y="0" width="100" height="50" />
-            </clipPath>
-            <linearGradient
-              id="grad1"
-              x1="0"
-              y1="50"
-              x2="100"
-              y2="50"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#FF5555" />
-              <stop offset="1" stopColor="#FF0000" />
-            </linearGradient>
-          </defs>
-
-          <circle cx="50" cy="50" r="50" fill="white" />
-          <circle
-            cx="50"
-            cy="50"
-            r="50"
-            fill="url(#grad1)"
-            clipPath="url(#half)"
-          />
-          <path
-            d="M50 35c8.284 0 15 6.716 15 15s-6.716 15-15 15-15-6.716-15-15 6.716-15 15-15z"
-            fill="white"
-            stroke="black"
-            strokeWidth="2.5"
-          />
-          <circle cx="50" cy="50" r="10" fill="black" />
-        </svg>
-      </div>
+    <div
+      className={cn(
+        "flex h-screen flex-col items-center justify-center",
+        className,
+      )}
+    >
+      <img
+        src="/pikachu-loading.gif"
+        alt="Pikachu having fun"
+        className={"transition-opacity duration-500 "}
+      />
     </div>
   );
 };

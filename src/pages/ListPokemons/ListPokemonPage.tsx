f5ff05935632da6167b80b2ee097c4ddb6a63375
SundayPowerEndre <sundaypeace@duck.com>
2023-12-28 16:49:49 +0100

    move the search bar to the bottom

--- src/pages/ListPokemons/ListPokemonPage.tsx
+++ src/pages/ListPokemons/ListPokemonPage.tsx
@@ -1,7 +1,7 @@
 import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
 import { getAllPokemonOptions } from "@/hooks/usePokemon";
 import ListItem from "./ListItem";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { SearchBar } from "@/components/SearchBar";
 import { mightBePokemon } from "@/lib/utils";
 
@@ -14,40 +14,37 @@ export const ListPokemon = () => {
     pageData.results.filter((item) => mightBePokemon(item.name, searchBarText)),
   );
 
-  useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const handleScroll = (_: unknown) => {
-      const { scrollTop, clientHeight, scrollHeight } =
-        document.documentElement;
-      const currentPos = scrollHeight - scrollTop;
-      const bottom = clientHeight;
-      const isNearBottom = currentPos < bottom + 100;
-      if (isNearBottom) {
-        if (hasNextPage) {
-          fetchNextPage();
-        }
+  const handleScroll = (event: React.UIEvent<HTMLElement>) => {
+    const { scrollTop, clientHeight, scrollHeight } = event.currentTarget;
+    const currentPos = scrollHeight - scrollTop;
+    const bottom = clientHeight;
+    const isNearBottom = currentPos < bottom + 100;
+    if (isNearBottom) {
+      if (hasNextPage) {
+        fetchNextPage();
       }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [hasNextPage, fetchNextPage]);
+    }
+  };
 
   return (
-    <div className="flex flex-col">
-      <SearchBar
-        fullWidth
-        value={searchBarText}
-        setValue={setSearchBarText}
-        setActive={() => {}}
-      />
-      <article className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 self-start pt-[10%]">
+    <div className="flex flex-col justify-between">
+      <article
+        className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 self-start overflow-auto max-h-[calc(100vh-184px)] min-h-6 "
+        onScroll={handleScroll}
+      >
         {filteredPokemon?.map((item) => (
           <ListItem item={item} key={item.url} />
         ))}
         {isFetchingNextPage && <div className=" my-6">Loading more...</div>}
       </article>
+      <div className="sticky bottom-11 ">
+        <SearchBar
+          fullWidth
+          value={searchBarText}
+          setValue={setSearchBarText}
+          setActive={() => {}}
+        />
+      </div>
     </div>
   );
 };

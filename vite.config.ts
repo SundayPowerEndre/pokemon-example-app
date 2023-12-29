3cb44ae4672b7a7b37d4ac71aa234ed5074d1734
SundayPowerEndre <sundaypeace@duck.com>
2023-12-29 20:58:58 +0100

    Adds nice vite stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,23 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { checker } from "vite-plugin-checker";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "src/**/*.+(js|jsx|ts|tsx)"',
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: { port: 1111, host: "0.0.0.0" },
 });

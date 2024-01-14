809b32b9cad202e79d2041f241b08d5c92b61666
SundayPowerEndre <sundaypeace@duck.com>
2024-01-14 22:17:07 +0100

    Better mobile experience triggering marquee

--- src/components/ClickableCard.tsx
+++ src/components/ClickableCard.tsx
@@ -3,18 +3,20 @@ import { useState, useRef } from "react";
 import Marquee from "react-fast-marquee";
 
 const ClickableCard = ({ onClick, children }: Props) => {
-  const [isHovering, setIsHovering] = useState(false);
+  const [isActive, setIsActive] = useState(false);
   const divRef = useRef<HTMLDivElement | null>(null);
   const isOverflowing = useIsOverflow(divRef);
 
-  const shouldShowMarquee = isHovering && isOverflowing;
+  const shouldShowMarquee = isActive && isOverflowing;
   const marqueeText = children.padEnd(50, "\u00A0")
 
   return (
     <button
       onClick={onClick}
-      onMouseEnter={() => setIsHovering(true)}
-      onMouseLeave={() => setIsHovering(false)}
+      onMouseEnter={() => setIsActive(true)}
+      onMouseLeave={() => setIsActive(false)}
+      onTouchStart={() => setIsActive(true)}
+      onTouchEnd={() => setIsActive(false)}
       className="rounded-md  border border-brand-secondary p-4 drop-shadow-lg "
     >
       {!shouldShowMarquee && (
@@ -26,7 +28,7 @@ const ClickableCard = ({ onClick, children }: Props) => {
         </div>
       )}
       {shouldShowMarquee && (
-        <Marquee autoFill={true} className="h-7 text-lg ">
+        <Marquee className="h-7 text-lg ">
           <p>{marqueeText}</p>
         </Marquee>
       )}
